2cb4a2101ccbd3b81554e4d86af4be9e21869a25
lymims <lymims@outlook.com>
2022-11-27 21:36:33 +0800

    调整框架和首页样式

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Learn</h1>
+  <div class="root">
+    <h1>Learn</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped lang="scss">
+.root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
 </style>
