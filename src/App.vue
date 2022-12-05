46df4cc4831b4a7fcad313182492615f5790ab15
lymimt <lymimt@outlook.com>
2022-12-05 05:04:41 +0000

    路由为 / 时设置首页菜单项为选中状态

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 
 <script setup lang="ts">
 import { HomeFilled } from '@element-plus/icons-vue';
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
@@ -25,11 +25,15 @@ onMounted(() => {
   // 因为未知原因，刷新页面或导航时，此处直接读取 route.path 的结果总是 '/'，
   // 在 setTimeout 中则可以读取到正确地址，即使设置的延时是 0
   setTimeout(() => {
-    // 此处提取路径 /lv1path/lv2path 中的 lv1path 部分
-    var lv1Path = route.path.split('/')[1];
-    // 当路径有效而选中菜单无效时，应用路径中的选中项
-    if (lv1Path && !activeIndex.value) {
-      activeIndex.value = `/${lv1Path}`;
+    if (route.path == '/') {
+      activeIndex.value = `/`;
+    } else {
+      // 此处提取路径 /lv1path/lv2path 中的 lv1path 部分
+      var lv1Path = route.path.split('/')[1];
+      // 当路径有效而选中菜单无效时，应用路径中的选中项
+      if (lv1Path && !activeIndex.value) {
+        activeIndex.value = `/${lv1Path}`;
+      }
     }
   }, 0);
 })
