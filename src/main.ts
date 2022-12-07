2ce0d177054afa8f17d459b12b6e4fe8471380da
lymimt <lymimt@outlook.com>
2022-12-07 09:21:39 +0000

    调整路由，将 Practice、Snippet 合并到 Guide

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteLocation } from 'vue-router';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 
 import Guide from './pages/guide/Guide.vue';
 import Home from './pages/Home.vue';
-import Practice from './pages/practice/Practice.vue';
-import Snippet from './pages/snippet/Snippet.vue';
 
 const routes = [
   { path: '', component: Home },
-  { path: '/guide', redirect: '/guide/README' },
-  { path: '/guide/:docName', component: Guide },
-  { path: '/practice', redirect: '/practice/README' },
-  { path: '/practice/:docName', component: Practice },
-  { path: '/snippet', redirect: '/snippet/README' },
-  { path: '/snippet/:docName', component: Snippet },
+  {
+    path: '/:group(guide|practice|snippet)',
+    redirect: (to: RouteLocation) => ({ path: `/${to.params.group}/README` })
+  },
+  { path: '/:group(guide|practice|snippet)/:docName', component: Guide },
 ];
 
 const router = createRouter({
--- src/pages/guide/Guide.vue
+++ src/pages/guide/Guide.vue
@@ -2,7 +2,7 @@
   <div class="sidebar">
     <li v-for="docname of docs">
       <!-- 使用 router-link 可以在不重新加载页面的情况下更改 URL -->
-      <router-link :to="`/guide/${docname}`">{{ docname }}</router-link>
+      <router-link :to="`/${group}/${docname}`">{{ docname }}</router-link>
     </li>
   </div>
   <main class="page-content">
@@ -16,26 +16,57 @@ import { useRoute } from 'vue-router';
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
+const group = ref('guide');
+const docName = ref('README');
 const doctext = ref('');
 
 const docs = ref([
   'README',
-  'markdown',
-  'mermaid',
-])
+]);
+
+const allDocs = {
+  'guide': [
+    'README',
+    'markdown',
+    'mermaid',
+  ],
+  'practice': [
+    'README',
+  ],
+  'snippet': [
+    'README',
+    'Vs-CodeSnippet',
+    'WPF-ButtonStyle',
+    'WPF-ColorPalette',
+    'WPF-FindAncestor',
+    'WPF-TreeView',
+    'WPF-UserControl',
+    'CSharp-ReadCSV',
+  ]
+};
 
 const route = useRoute();
 
 // 初始化时加载文档
 onMounted(async () => {
-  await updateDoc(route.params.docName);
+  group.value = route.params.group;
+  docName.value = route.params.docName;
+  await updateDoc();
 });
 
 // 路由时加载文档
-watch(() => route.params.docName, updateDoc);
+watch(() => route.params.group, g => {
+  group.value = g;
+  docs.value = allDocs[g];
+  updateDoc();
+});
+watch(() => route.params.docName, doc => {
+  docName.value = doc;
+  updateDoc();
+});
 
-async function updateDoc(docName: string | string[]) {
-  let t = await fetch(`/guide/${docName}.md`);
+async function updateDoc() {
+  let t = await fetch(`/${group.value}/${docName.value}.md`);
   doctext.value = await t.text();
 }
 </script>
