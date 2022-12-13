5f7dd651b312d72c8f6da2ae76bed943cc167d3e
lymims <lymims@outlook.com>
2022-12-14 00:18:04 +0800

    调试时将服务暴露到更大的局域网范围

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 添加此行，可以将服务暴露到整个局域网
+  server: { host: '0.0.0.0' },
 })
